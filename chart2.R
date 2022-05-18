92746bfe102045ff3ddb4f6ac04dae0332b200de
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2022-05-18 16:10:48 -0700

    Added files and completed chart1

---
+++ chart2.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+
+
+# 2 differnt continous variablers are realted to one another 
+
+# Need a graph that can do that 
+
+
+prison_pop <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true", stringsAsFactors = FALSE)
+
+View(prison_pop)
