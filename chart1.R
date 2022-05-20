4948bee74df4d5ae51bc196beb603c069695884b
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2022-05-20 01:51:15 -0700

    Fixed styling almost done

--- chart1.R
+++ chart1.R
@@ -2,7 +2,12 @@ library(dplyr)
 library(ggplot2)
 library(plotly)
 
-prison_pop <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true", stringsAsFactors = FALSE)
+url_to_read <- paste0(
+  "https://github.com/melaniewalsh/Neat-Datasets",
+  "/blob/main/us-prison-pop.csv?raw=true"
+)
+
+prison_pop <- read.csv(url_to_read, stringsAsFactors = FALSE)
 
 
 prision_pop_wa <- prison_pop %>% filter(state == "WA")
@@ -16,17 +21,22 @@ prision_pop_wa <- prision_pop_wa %>% filter(year >= "2000" & year <= "2018")
 filtered_data <- prision_pop_wa %>% group_by(year, county_name)
 
 
-chart1 <- ggplot(filtered_data, aes(year, colour=Race)) +
+chart1 <- ggplot(filtered_data, aes(year, colour = Race)) +
   geom_line(aes(y = aapi_prison_pop, color = "Asian or Pacific Islander")) +
   geom_line(aes(y = black_prison_pop, color = "Black")) +
   geom_line(aes(y = latinx_prison_pop, color = "Latinx")) +
   geom_line(aes(y = native_prison_pop, color = "Native American")) +
   geom_line(aes(y = other_race_prison_pop, color = "Other/Unknown")) +
   geom_line(aes(y = white_prison_pop, color = "White")) +
-  scale_color_manual(values = c("Black", "Red", "Green", "Blue", "Purple", "Brown")) +
-  labs(title = "The Different Population Counts For Different Races in King County Prison Over 2000 to 2018", y = "Count", x = "Year") +
+  scale_color_manual(values = c(
+    "Black", "Red", "Green",
+    "Blue", "Purple", "Brown"
+  )) +
+  labs(title = paste0(
+    "The Different Population Counts For Different",
+    "Races in King County Prison Over 2000 to 2018"
+  ), y = "Count", x = "Year") +
   scale_x_continuous(breaks = seq(1970, 2018, by = 5))
 
-  
+
 ggplotly(chart1)
-  
--- map.R
+++ map.R
@@ -3,23 +3,33 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
+url_to_read <- paste0(
+  "https://github.com/melaniewalsh/Neat-Datasets",
+  "/blob/main/us-prison-pop.csv?raw=true"
+)
 
-
-
-prison_pop <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true", stringsAsFactors = FALSE)
+prison_pop <- read.csv(url_to_read, stringsAsFactors = FALSE)
 
 
 prison_pop <- prison_pop %>% filter(year == "2000")
 
-prison_pop_wa <- prison_pop %>% filter(state == "WA")
+prison_pop_wa <- prison_pop %>%
+  filter(state == "WA") %>%
+  rename("County" = county_name) %>%
+  rename("Prison" = total_prison_pop) %>%
+  rename("Population" = total_pop)
 
 
-map_map <- plot_usmap("states", include = "Washington", data = prison_pop_wa, values = "total_prison_pop", text = paste("County: ", prison_pop_wa$county_name)) +
-  scale_fill_continuous(low = "darkturquoise", high = "orange", name = "Number of prisoners") +
-  labs(title= "Number of Prisoners in Each County in Washington in 2000") 
+map_map <- plot_usmap("states",
+  include = "Washington",
+  data = prison_pop_wa, values = "Prison"
+) +
+  scale_fill_continuous(
+    low = "darkturquoise",
+    high = "orange", name = "Number of prisoners"
+  ) +
+  labs(title = "Number of Prisoners in Each County in Washington in 2000") +
+  aes(text = paste(County, "\nPopulation: ", Population))
 
 
-
-ggplotly(map_map, tooltip = "text")
-
-
+ggplotly(map_map)
